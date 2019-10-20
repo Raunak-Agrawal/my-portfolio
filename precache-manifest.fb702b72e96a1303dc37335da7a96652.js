247d153cd19027e80665fdc3ffabb564e42f9056
Raunak-Agrawal <raunk325@gmail.com>
2019-10-20 16:45:16 +0530

    Updates

---
+++ precache-manifest.fb702b72e96a1303dc37335da7a96652.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d3fd89e1089277193168810d4def63",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "637a11616525f6d440f7",
+    "url": "/my-portfolio/static/css/main.dfb0c7ef.chunk.css"
+  },
+  {
+    "revision": "8f34a438f0db978d7353",
+    "url": "/my-portfolio/static/js/2.15320322.chunk.js"
+  },
+  {
+    "revision": "637a11616525f6d440f7",
+    "url": "/my-portfolio/static/js/main.63e3e6cd.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "98e5372c0c2705e17a05303e8efa7f99",
+    "url": "/my-portfolio/static/media/RaunakAgrawal.98e5372c.jpeg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/my-portfolio/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/my-portfolio/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/my-portfolio/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/my-portfolio/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "d286adcf8ae20035e45202a2db88804e",
+    "url": "/my-portfolio/static/media/raunakresume.d286adcf.pdf"
+  }
+]);
