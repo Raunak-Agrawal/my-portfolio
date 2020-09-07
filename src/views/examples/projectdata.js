ba180fba532cfe892792921a0f9fb70b31f81101
Raunak Agrawal <35456434+Raunak-Agrawal@users.noreply.github.com>
2020-09-07 22:51:33 +0545

    Update projectdata.js

--- src/views/examples/projectdata.js
+++ src/views/examples/projectdata.js
@@ -1,21 +1,31 @@
 export const projects = [
   {
+    name: "Amazon Insurance product",
+    description:
+      "Working on amazonpay insurance team to build auto insurance experience using React, AWS. Major work includes designing and developing front end experience and building High-level and Low-level designs for product." 
+  },
+   {
+    name: "Onboarding app",
+    description:
+      "Worked solely on a building an onboarding app for sales team for a client using React Native and AWS. Delivered the project before the deadline. "
+  },
+  {
     name: "Operations Management System",
     description:
-      "Working on a hybrid app for a SAAS product for US region.An application to streamline internal operations management in Hotel chains in the USA, Core Features: Task Mgmt.,Employee Mgmt., QR Code based Cloak-in/Out, Employee Schedule Mgmt., Inventory etc",
+      "Worked on a hybrid app for a SAAS product for US region.An application to streamline internal operations management in Hotel chains in the USA, Core Features: Task Mgmt.,Employee Mgmt., QR Code based Cloak-in/Out, Employee Schedule Mgmt., Inventory etc",
     techStack: "React JS, React Native, Redux"
   },
   {
     name: "E-commerce clothing store",
     description:
-      "Working in creating a dummy clothing store but enhancing my exposure to ReactJS by integrating some cool features such as Redux, Firebase, Styled Components, Redux Saga, Hooks, Context API along with boosting performance and optimizing production build",
+      "Worked in creating a dummy clothing store but enhancing my exposure to ReactJS by integrating some cool features such as Redux, Firebase, Styled Components, Redux Saga, Hooks, Context API along with boosting performance and optimizing production build",
     techStack: "React.JS, Firebase, Redux, Styled Components, Webpack",
     link: "https://shop-clothing123.herokuapp.com/"
   },
   {
     name: "Custom Clothing",
     description:
-      "Working in transforming an online tailoring website, which is in WordPress to a React-Redux based frontend and NodeJS based backend product",
+      "Worked in transforming an online tailoring website, which is in WordPress to a React-Redux based frontend and NodeJS based backend product",
     techStack: "ReactJS, NodeJS, MongoDB"
   },
   {
